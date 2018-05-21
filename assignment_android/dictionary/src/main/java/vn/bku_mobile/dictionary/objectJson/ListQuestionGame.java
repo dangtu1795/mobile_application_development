8d76b4dae3af594eb66026bbf738df85d8e5ef0d
Truong Dang <dangdzuytruong@gmail.com>
2018-05-21 22:18:47 +0700

    update Assignment Branch
    + Add java Quiz Game
    - Modify Game Item

---
+++ assignment_android/dictionary/src/main/java/vn/bku_mobile/dictionary/objectJson/ListQuestionGame.java
@@ -0,0 +1,24 @@
+package vn.techlove.dictionary.objectJson;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+/**
+ * Created by TruongDzuy on 5/20/2018.
+ */
+
+public class ListQuestionGame {
+    @SerializedName("data")
+    @Expose
+    private List<Question> list_question;
+    public List<Question> getList_question() {
+        return list_question;
+    }
+
+    public static ListQuestionGame instance;
+    public static void setListQuestionGame(ListQuestionGame list){
+        instance = list;
+    }
+}
